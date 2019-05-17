426345c707d5b4597d8e05ffbd187dd9d6209dd6
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-05-17 13:19:31 -0700

    added switch

--- liri.js
+++ liri.js
@@ -6,8 +6,31 @@ var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+var axios = require("axios");
+
 //vars to capture user inputs.
 var user = process.argv[2];
 var input = process.argv[3];
+
+switch (user) {
+    case "concert-this":
+        searchForBandsInTown(input);
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong(input);
+        break;
+
+    case "movie-this":
+        movieThis(input);
+        break;
+
+    case "do-what-it-says":
+        theRandom();
+        break;
+
+
+}
 
 
