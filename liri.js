8b9c473c2dbf480af2c4550ac7ce73414b8eee37
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-05-17 14:00:12 -0700

    some functions

--- liri.js
+++ liri.js
@@ -29,8 +29,64 @@ switch (user) {
     case "do-what-it-says":
         theRandom();
         break;
-
-
 }
 
+// All the functions for the keys
+
+function searchForBandsInTown(artist) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response) {
+            if (response.data[0].venue != undefined) {
+                console.log("Event Venue: " + response.data[0].venue.name);
+                console.log("Event Location: " + response.data[0].venue.city);
+                var eventDateTime = moment(response.data[0].datetime);
+                console.log("Event Date & Time" + eventDateTime.format("dddd,MMMM Do YYYY"));
+
+            }
+            else {
+                console.log("No results found.");
+            }
+        }
+    ).catch(function (error) {
+        console.log(error);
+    });
+}
+
+function spotifyThisSong(song) {
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            if (response.tracks.total === 0) {
+                errorConditionForSpotify();
+            } else {
+                console.log("Artist: " + response.tracks.items[0].artist[0].name);
+                console.log("Track: " + response.tracks.items[0].name);
+                console.log("Preview URL: " + response.tracks.items[0].preview_url);
+                console.log("Album: " + response.tracks.items[0].album.name);
+            }
+        }).catch(function (error) {
+            console.log(error);
+            console.log("No Results found. Showing results for 'The Sign' by Ace of Base");
+        });
+}
+
+function errorConditionForSpotify() {
+    spotify
+        .search({ type: 'track', query: 'The Sign' })
+        .then(function (response) {
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                if (response.tracks.items[i].artists[0].name === "Ace of Base") {
+                    console.log("Artist: " + response.tracks.items[i].artist[0].name);
+                    console.log("Track: " + response.tracks.items[i].name);
+                    console.log("Preview URL: " + response.tracks.items[i].preview_url);
+                    console.log("Album: " + response.tracks.items[i].album.name);
+                    i = response.tracks.items.length;
+                }
+            }
+        }).catch(function (error) {
+            console.log(error);
+            console.log("No Results found. ");
+        });
+}
 
